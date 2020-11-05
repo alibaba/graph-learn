400ce5c17be4fa303d2d084580a099b0ae873beb
xiaoming-qxm <xiaoming.qxm@alibaba-inc.com>
2020-11-05 18:14:30 +0800

    Compatible with centos-7

--- env.sh
+++ env.sh
@@ -1,5 +1,6 @@
 ROOT=`pwd`
 GRPC_LIB=${ROOT}/third_party/grpc/build/lib
 GFLAGS_LIB=${ROOT}/third_party/gflags/build/lib
-LD_LIBRARY_PATH=${ROOT}/built/lib:${GRPC_LIB}:${GFLAGS_LIB}:$LD_LIBRARY_PATH
+SYS_INSTALL_LIB=/usr/local/lib:/usr/local/lib64
+LD_LIBRARY_PATH=${SYS_INSTALL_LIB}:${ROOT}/built/lib:${GRPC_LIB}:${GFLAGS_LIB}:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
