31f563b6d6b03f8458149339f314029de9fb99a1
qiaozi.zwb <qiaozi.zwb@alibaba-inc.com>
2020-11-20 10:43:22 +0800

    update link prediction

--- examples/tf/graphsage/link_prediction.py
+++ examples/tf/graphsage/link_prediction.py
@@ -32,7 +32,6 @@ os.system('rm -rf %s*' % TRACKER_PATH)
 
 
 def train(config, graph):
-  print('start training....')
   def model_fn():
     return GraphSage(graph,
                      config['class_num'],
@@ -48,51 +47,45 @@ def train(config, graph):
                      agg_type=config['agg_type'],
                      node_type=config['node_type'],
                      edge_type=config['edge_type'],
-                     train_node_type=config['node_type'])
+                     train_node_type=config['train_node_type'])
   trainer = gl.LocalTFTrainer(model_fn,
                               epoch=config['epoch'],
                               optimizer=gl.get_tf_optimizer(
                                   config['learning_algo'],
                                   config['learning_rate'],
                                   config['weight_decay']))
-
+  print("start to train...")
   trainer.train()
   embs = trainer.get_node_embedding()
-  # print(embs.shape)
+  print("shape:", embs.shape)
   np.save(config['emb_save_dir'], embs)
+
 
 from sklearn.metrics.pairwise import cosine_similarity
 def test(config, graph):
-
-  sampler_node = graph.node_sampler(config['node_type'], 1)
-  nodes_set = set(sampler_node.get().ids)
-  for i in range(10):
-      nodes_list = sampler_node.get().ids
-      nodes_set_tmp = set(nodes_list)
-      nodes_set = nodes_set | nodes_set_tmp
-
-  id_node = list(sorted(nodes_set))
-  node_id = {}
-  for i in range(len(id_node)):
-      node_id[id_node[i]] = i
-
-  test_count = 1.0
+  test_count = 0.0
   find_count = 0.0
   embs = np.load(config['emb_save_dir'] + ".npy")
+  id_map = {}
+  for i in range(embs.shape[0]):
+      id_map[embs[i][0]] = i
   sampler_edge = graph.edge_sampler(config['edge_type'], 1000)
   edges_list = sampler_edge.get()
   src_ids = edges_list.src_ids
   dst_ids = edges_list.dst_ids
   for j in range(len(src_ids)):
-      if src_ids[j] in node_id and dst_ids[j] in node_id:
-          vec_a = embs[node_id[src_ids[j]], :]
-          vec_b = embs[node_id[dst_ids[j]], :]
+      if True:
+          vec_a = embs[id_map[src_ids[j]]][1:]
+          vec_b = embs[id_map[dst_ids[j]]][1:]
           num = float(np.sum(vec_a * vec_b))
           denom = np.linalg.norm(vec_a) * np.linalg.norm(vec_b)
-          cos = num / denom
+          if denom == 0:
+              cos = 0
+          else:
+              cos = num / denom
           cos_sim = 0.5 + 0.5 * cos
           test_count = test_count + 1
-          if cos_sim >= 0.1:
+          if cos_sim >= 0.7:
               find_count = find_count + 1
 
   acc = find_count/test_count
@@ -128,16 +121,12 @@ def main():
             'neg_num': 10,
             'emb_save_dir': './id_emb',
             'node_type': node_type,
-            'edge_type': edge_type}
+            'edge_type': edge_type,
+            'train_node_type': node_type}
 
-
-  gl.set_vineyard_graph_id(obj['vineyard_id'])
-  gl.set_vineyard_ipc_socket(obj['vineyard_socket'])
-  gl.set_storage_mode(8)
   g = gl.get_graph_from_handle(handle, worker_index=task_index, worker_count=task_count, standalone=True)
 
   train(config, g)
-
   test(config, g)
 
 
