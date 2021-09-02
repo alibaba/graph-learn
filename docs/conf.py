1cf344be5db47ebb638905d6a476b33b8e0ebb4e
Seventeen17 <17aloha@gmail.com>
2021-09-02 19:27:18 +0800

    Support markdown tables in readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ release = u'1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_markdown_tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
