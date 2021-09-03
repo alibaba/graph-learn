b788832ac394e7a098fd03077e58deaee3d8c634
Seventeen17 <17aloha@gmail.com>
2021-09-03 18:52:18 +0800

    Fix zh_CN docs config

--- docs/zh_CN/conf.py
+++ docs/zh_CN/conf.py
@@ -63,7 +63,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = u'cn'
+language = u'zh_CN'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -92,7 +92,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['../mages']
+html_static_path = ['../images']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
