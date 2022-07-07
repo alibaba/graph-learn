32af5ef86d47c477cb610a2ae30a2a076e435763
goldenleaves <houbai.zzc@alibaba-inc.com>
2022-07-07 09:14:55 +0000

    fix

--- dynamic-graph-service/run_unittests.sh
+++ dynamic-graph-service/run_unittests.sh
@@ -15,15 +15,21 @@ built_bin_dir=${script_dir}/built/bin
 ########################################################
 function deploy_kafka_cluster() {
   mkdir -p "${build_dir}"/kafka_cluster
-  rm -rf "${build_dir}"/kafka_cluster/*
   pushd "${build_dir}"/kafka_cluster
-  wget -q https://graphlearn.oss-cn-hangzhou.aliyuncs.com/package/kafka_2.13-3.0.0.tgz
-  tar -xvzf kafka_2.13-3.0.0.tgz
+  if [ ! -f "kafka_2.13-3.0.0.tgz" ]; then
+    wget -q https://graphlearn.oss-cn-hangzhou.aliyuncs.com/package/kafka_2.13-3.0.0.tgz
+  fi
+  if [ ! -d "kafka_2.13-3.0.0" ]; then
+    tar -xvzf kafka_2.13-3.0.0.tgz
+  fi
   popd
 
   pushd "${build_dir}"/kafka_cluster/kafka_2.13-3.0.0
   ./bin/zookeeper-server-start.sh config/zookeeper.properties &
+  sleep 5s
   ./bin/kafka-server-start.sh config/server.properties &
+  sleep 5s
+
   for topic in record-poller-ut-1 record-poller-ut-2 sampling-actor-ut sample-publisher-ut service-record-polling-ut service-sample-publishing-ut
   do
     ./bin/kafka-topics.sh --create --topic $topic --bootstrap-server localhost:9092 --partitions 4 --replication-factor 1
@@ -39,7 +45,9 @@ function deploy_kafka_cluster() {
 function stop_kafka_cluster() {
   pushd "${build_dir}"/kafka_cluster/kafka_2.13-3.0.0
   ./bin/kafka-server-stop.sh config/server.properties
+  sleep 2s
   ./bin/zookeeper-server-stop.sh config/zookeeper.properties
+  sleep 2s
   rm -rf /tmp/kafka-logs
   rm -rf /tmp/zookeeper
   popd
@@ -67,15 +75,18 @@ function run_ut() {
 # Arguments:
 #   None
 ########################################################
-function make_code_cov() {
+function make_code_cov_reports() {
   pushd "${build_dir}"
   make code_coverage
-  lcov --capture --directory ./coverage --output-file coverage.info
-  genhtml coverage.info --output-directory ../gcov-reports
+  pushd "${build_dir}"/coverage
+  lcov --capture --directory ./targets --output-file coverage.info
+  lcov --remove coverage.info '/usr/include/*' '*third_party/*' -o coverage.info
+  genhtml coverage.info --output-directory ./reports
+  popd
   popd
 }
 
 deploy_kafka_cluster
 run_ut
 stop_kafka_cluster
-make_code_cov
+make_code_cov_reports
